d5573d84d4ca1133a49322df62cfa4c6c283920c
Percy Ghenburg <41657434+WriterArtistCoder@users.noreply.github.com>
2019-04-03 13:47:13 -0700

    Create BandNameMaker.java

---
+++ BandNameMaker.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class BandNameMaker {
+    public static void main(String args[]) {
+        String[] nouns = {
+            "Noah", "the Noahs", "Barbie", "the Barbies", "the Nuclear Bomb", "the Nuclear Bombs", "Barney", "the Barnies", "Harry Potter", "the Harry Potters",
+            "Homer Simpson", "the Homer Simpsons", "the Simpson", "the Simpsons", "the Beatle", "the Beatles", "the Beetle", "the Beetles", "Percy Jackson",
+            "the Percy Jacksons", "My Little Pony", "my Little Ponies", "Phineas", "the Phineasi", "Ferb", "the Ferbs"
+        };
+        
+        String noun1 = nouns[new Random().nextInt(nouns.length)];
+        String noun2 = nouns[new Random().nextInt(nouns.length)];
+        
+        noun1 = noun1.substring(0, 1).toUpperCase() + noun1.substring(1);
+
+        System.out.println(">> GENERATED BAND NAME: " + noun1 + " and " + noun2);
+        System.out.println(">> ALL POSSIBILITIES: " + nouns.length*nouns.length);
+        
+        try {
+            Runtime.getRuntime().exec("speak " + noun1 + " and " + noun2);
+        } catch (Exception e) {
+            System.out.println(">> TEXT-TO-SPEECH ALGORITHM NOT COOPERATING");
+        }
+    }
+}
